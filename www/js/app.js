af9ada511488a93193954b2a895896c6c7ede566
Raymond Carino <sloooooowduck@gmail.com>
2014-12-21 02:21:39 -0800

    angular material

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'timer'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'timer', 'ngMaterial'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
